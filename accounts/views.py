0aef703f33b92b3a3f92f10b7ba73d2d9102dbdb
seoyoung <1203sylim@gmail.com>
2025-03-26 13:59:11 +0900

    로그인 유무에 따른 nav 구조 변경

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
-from django.contrib.auth import login as auth_login # 장고의 login 함수 이름을 auth_login로 바꾸기(이름 중복 때문)
+from django.contrib.auth import login as auth_login  # 장고의 login 함수 이름을 auth_login로 바꾸기(이름 중복 때문)
+from django.contrib.auth import logout as auth_logout
 # Create your views here.
 
 def signup(request):
@@ -22,7 +23,7 @@ def login(request):
         form = CustomAuthenticationForm(request, request.POST)
         if form.is_valid(): # 유저정보는 저장할 필요가 없기 때문에 save 안함
              auth_login(request, form.get_user())
-             return redirect('articles:index')
+             return redirect('accounts:login')
     else:
         form = CustomAuthenticationForm()
 
@@ -31,3 +32,7 @@ def login(request):
     }
 
     return render(request, 'login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')
