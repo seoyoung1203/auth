46f28321927baeb1fdffdeea8ffd54dcfa8ef16b
seoyoung <1203sylim@gmail.com>
2025-03-26 10:18:37 +0900

    모델링/번역본 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+# Django의 기본 User 모델(auth.User)이 동시에 존재하기 때문에 충돌 발생
